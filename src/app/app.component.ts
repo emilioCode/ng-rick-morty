c16f9f411bccc8c5548ad6436b62fbde1be2a485
emilioCode <emilio_mem@hotmail.com>
2023-08-21 14:32:15 -0400

    Home Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-rick-morty';
+
+  constructor(private sanitizer: DomSanitizer) { }
+
+  getAssetImage(): SafeUrl {
+    const imageUrl = '../assets/images/rick-morty.png'; // Path to your image in the assets folder
+    return this.sanitizer.bypassSecurityTrustUrl(imageUrl);
+  }
 }
